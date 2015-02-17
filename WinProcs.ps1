a92ce8ff474d2756e53785b28520d932591321e8
Lee Farrar <Lee@boundary.com>
2015-02-17 18:44:34 +0000

    fixed process count for single processes

--- WinProcs.ps1
+++ WinProcs.ps1
@@ -3,15 +3,18 @@ while($true){
 	$process =get-process $processName | select CPU, VM, ID | Sort-Object VM
 	$procCpu=0
 	$procMem=0
+	$procCount=0
 	if ([bool]$process) { 
 		if($process -isnot [system.array]){
+			$procCount=1
 			$procCpu= $process | select -expand CPU
 			$procMem= $process | select -expand VM
 		}else{
+			$procCount=$process.Count
 			$procCpu= $process[0] | select -expand CPU
 			$procMem= $process[0] | select -expand VM
 		}
-		Write-Host "LFPROCCOUNT" $process.Count $processName
+		Write-Host "LFPROCCOUNT" $procCount $processName
 		Write-Host "LFPROCMEM" $procMem $processName
 		Write-Host "LFPROCCPU" $procCpu $processName
 		
